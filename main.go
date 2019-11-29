dcbe00fd5affe6322c63be922ba062590e206e62
Jorrit Salverda <jsalverda@travix.com>
2019-11-29 11:10:29 +0100

    cancel commands on sigterm

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
-	"os"
-	"os/exec"
 	"runtime"
-	"strings"
 
 	"github.com/alecthomas/kingpin"
+	foundation "github.com/estafette/estafette-foundation"
+	"github.com/rs/zerolog/log"
 )
 
 var (
+	appgroup  string
 	app       string
 	version   string
 	branch    string
@@ -33,80 +33,79 @@ func main() {
 	// parse command line parameters
 	kingpin.Parse()
 
-	// log to stdout and hide timestamp
-	log.SetOutput(os.Stdout)
-	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+	// init log format from envvar ESTAFETTE_LOG_FORMAT
+	foundation.InitLoggingFromEnv(appgroup, app, version, branch, revision, buildDate)
 
-	// log startup message
-	logInfo("Starting %v version %v...", app, version)
+	// create context to cancel commands on sigterm
+	ctx := foundation.InitCancellationContext(context.Background())
 
-	logInfo("Unmarshalling credentials parameter...")
+	log.Info().Msg("Unmarshalling credentials parameter...")
 	var credentialsParam CredentialsParam
 	err := json.Unmarshal([]byte(*paramsJSON), &credentialsParam)
 	if err != nil {
-		log.Fatal("Failed unmarshalling credential parameter: ", err)
+		log.Fatal().Err(err).Msg("Failed unmarshalling credential parameter")
 	}
 
-	logInfo("Validating required credential parameter...")
+	log.Info().Msg("Validating required credential parameter...")
 	valid, errors := credentialsParam.ValidateRequiredProperties()
 	if !valid {
-		log.Fatal("Not all valid fields are set: ", errors)
+		log.Fatal().Msgf("Not all valid fields are set: %v", errors)
 	}
 
-	logInfo("Unmarshalling injected credentials...")
+	log.Info().Msg("Unmarshalling injected credentials...")
 	var credentials []GKECredentials
 	err = json.Unmarshal([]byte(*credentialsJSON), &credentials)
 	if err != nil {
-		log.Fatal("Failed unmarshalling injected credentials: ", err)
+		log.Fatal().Err(err).Msg("Failed unmarshalling injected credentials")
 	}
 
-	logInfo("Checking if credential %v exists...", credentialsParam.Credentials)
+	log.Info().Msgf("Checking if credential %v exists...", credentialsParam.Credentials)
 	credential := GetCredentialsByName(credentials, credentialsParam.Credentials)
 	if credential == nil {
-		log.Fatalf("Credential with name %v does not exist.", credentialsParam.Credentials)
+		log.Fatal().Msgf("Credential with name %v does not exist.", credentialsParam.Credentials)
 	}
 
-	logInfo("Unmarshalling parameters / custom properties...")
+	log.Info().Msg("Unmarshalling parameters / custom properties...")
 	var params Params
 	err = json.Unmarshal([]byte(*paramsJSON), &params)
 	if err != nil {
-		log.Fatal("Failed unmarshalling parameters: ", err)
+		log.Fatal().Err(err).Msg("Failed unmarshalling parameters")
 	}
 
-	logInfo("Setting defaults for parameters that are not set in the manifest...")
+	log.Info().Msg("Setting defaults for parameters that are not set in the manifest...")
 	params.SetDefaults()
 
-	logInfo("Retrieving service account email from credentials...")
+	log.Info().Msg("Retrieving service account email from credentials...")
 	var keyFileMap map[string]interface{}
 	err = json.Unmarshal([]byte(credential.AdditionalProperties.ServiceAccountKeyfile), &keyFileMap)
 	if err != nil {
-		log.Fatal("Failed unmarshalling service account keyfile: ", err)
+		log.Fatal().Err(err).Msg("Failed unmarshalling service account keyfile")
 	}
 	var saClientEmail string
 	if saClientEmailIntfc, ok := keyFileMap["client_email"]; !ok {
-		log.Fatal("Field client_email missing from service account keyfile")
+		log.Fatal().Msg("Field client_email missing from service account keyfile")
 	} else {
 		if t, aok := saClientEmailIntfc.(string); !aok {
-			log.Fatal("Field client_email not of type string")
+			log.Fatal().Msg("Field client_email not of type string")
 		} else {
 			saClientEmail = t
 		}
 	}
 
-	logInfo("Storing gcs credential %v on disk...", credentialsParam.Credentials)
+	log.Info().Msgf("Storing gcs credential %v on disk...", credentialsParam.Credentials)
 	err = ioutil.WriteFile("/key-file.json", []byte(credential.AdditionalProperties.ServiceAccountKeyfile), 0600)
 	if err != nil {
-		log.Fatal("Failed writing service account keyfile: ", err)
+		log.Fatal().Err(err).Msg("Failed writing service account keyfile")
 	}
 
-	logInfo("Authenticating to google cloud")
-	runCommand("gcloud", []string{"auth", "activate-service-account", saClientEmail, "--key-file", "/key-file.json"})
+	log.Info().Msg("Authenticating to google cloud")
+	foundation.RunCommandWithArgs(ctx, "gcloud", []string{"auth", "activate-service-account", saClientEmail, "--key-file", "/key-file.json"})
 
-	logInfo("Setting gcloud account")
-	runCommand("gcloud", []string{"config", "set", "account", saClientEmail})
+	log.Info().Msg("Setting gcloud account")
+	foundation.RunCommandWithArgs(ctx, "gcloud", []string{"config", "set", "account", saClientEmail})
 
-	logInfo("Setting gcloud project")
-	runCommand("gcloud", []string{"config", "set", "project", credential.AdditionalProperties.Project})
+	log.Info().Msg("Setting gcloud project")
+	foundation.RunCommandWithArgs(ctx, "gcloud", []string{"config", "set", "project", credential.AdditionalProperties.Project})
 
 	// clean up old stuff
 	switch params.Action {
@@ -130,42 +129,8 @@ func main() {
 
 		cpArgs = append(cpArgs, params.Source, fmt.Sprintf("gs://%v/%v", params.Bucket, params.Destination))
 
-		runCommand("gsutil", cpArgs)
+		foundation.RunCommandWithArgs(ctx, "gsutil", cpArgs)
 
 		break
 	}
-}
-
-func handleError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func runCommand(command string, args []string) {
-	err := runCommandExtended(command, args)
-	handleError(err)
-}
-
-func runCommandExtended(command string, args []string) error {
-	logInfo("Running command '%v %v'...", command, strings.Join(args, " "))
-	cmd := exec.Command(command, args...)
-	cmd.Dir = "/estafette-work"
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	log.Println("")
-	return err
-}
-
-func getCommandOutput(command string, args []string) (string, error) {
-	logInfo("Getting output for command '%v %v'...", command, strings.Join(args, " "))
-	output, err := exec.Command(command, args...).Output()
-
-	return string(output), err
-}
-
-func logInfo(message string, args ...interface{}) {
-	formattedMessage := fmt.Sprintf(message, args...)
-	log.Printf("%v\n\n", formattedMessage)
 }
